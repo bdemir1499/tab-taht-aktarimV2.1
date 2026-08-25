a1a8b39037ab60e8da15bd4f701740d42605280f
bdemir1499 <b.demir1499@gmail.com>
2026-08-26 01:53:20 +0300

    sdcs

--- app.js
+++ app.js
@@ -8181,7 +8181,7 @@ tonyBtn.onclick = async () => {
                 const hand1 = results.multiHandLandmarks[0];
                 
                 // Lazer İmleci Çiz
-                const px1 = hand1[8].x * window.innerWidth;
+                const px1 = (1 - hand1[8].x) * window.innerWidth;
                 const py1 = hand1[8].y * window.innerHeight;
                 laserCursor.style.display = 'block';
                 laserCursor.style.left = px1 + 'px';
